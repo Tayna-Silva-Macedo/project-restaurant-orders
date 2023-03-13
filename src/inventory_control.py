2a4c4569f3b23f0cc286ee40e59197f40637997a
Tayna-Macedo <tayna_sm1996@hotmail.com>
2023-03-13 19:37:10 -0300

    feat: Implementação da classe InventoryControl que faz o controle de estoque, retornando o que deve ser comprado

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -1,25 +1,38 @@
 class InventoryControl:
     INGREDIENTS = {
-        'hamburguer': ['pao', 'carne', 'queijo'],
-        'pizza': ['massa', 'queijo', 'molho'],
-        'misto-quente': ['pao', 'queijo', 'presunto'],
-        'coxinha': ['massa', 'frango'],
+        "hamburguer": ["pao", "carne", "queijo"],
+        "pizza": ["massa", "queijo", "molho"],
+        "misto-quente": ["pao", "queijo", "presunto"],
+        "coxinha": ["massa", "frango"],
     }
     MINIMUM_INVENTORY = {
-        'pao': 50,
-        'carne': 50,
-        'queijo': 100,
-        'molho': 50,
-        'presunto': 50,
-        'massa': 50,
-        'frango': 50,
+        "pao": 50,
+        "carne": 50,
+        "queijo": 100,
+        "molho": 50,
+        "presunto": 50,
+        "massa": 50,
+        "frango": 50,
     }
 
     def __init__(self):
-        pass
+        self.shopping_list = {
+            "pao": 0,
+            "carne": 0,
+            "queijo": 0,
+            "molho": 0,
+            "presunto": 0,
+            "massa": 0,
+            "frango": 0,
+        }
 
     def add_new_order(self, customer, order, day):
-        pass
+        for ingredient in self.INGREDIENTS[order]:
+            if (
+                self.shopping_list[ingredient]
+                < self.MINIMUM_INVENTORY[ingredient]
+            ):
+                self.shopping_list[ingredient] += 1
 
     def get_quantities_to_buy(self):
-        pass
+        return self.shopping_list
