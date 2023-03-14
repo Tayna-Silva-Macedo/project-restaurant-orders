a3d08614e7d94718e70ba83cc540a7a455988d9a
Tayna-Macedo <tayna_sm1996@hotmail.com>
2023-03-14 15:23:09 -0300

    feat: Implementação do método get_available_dishes que retorna os pratos disponíveis

--- src/inventory_control.py
+++ src/inventory_control.py
@@ -26,6 +26,8 @@ class InventoryControl:
             "frango": 0,
         }
 
+        self.inventory = self.MINIMUM_INVENTORY.copy()
+
     def add_new_order(self, customer, order, day):
         for ingredient in self.INGREDIENTS[order]:
             if (
@@ -33,6 +35,24 @@ class InventoryControl:
                 < self.MINIMUM_INVENTORY[ingredient]
             ):
                 self.shopping_list[ingredient] += 1
+                self.inventory[ingredient] -= 1
+            else:
+                return False
 
     def get_quantities_to_buy(self):
         return self.shopping_list
+
+    def get_available_dishes(self):
+        missing_ingredients = {
+            ingredient
+            for ingredient in self.inventory
+            if self.inventory[ingredient] < 1
+        }
+
+        available_dishes = {
+            dish
+            for dish, ingredients in self.INGREDIENTS.items()
+            if len(set(ingredients).intersection(missing_ingredients)) == 0
+        }
+
+        return available_dishes
